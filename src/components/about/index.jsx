15d4fe747045214b414d27cc4b4f8c37fbcac94e
Arshad Pakkali <arshadpakkali@gmail.com>
2021-12-11 11:11:00 +0530

    updated my data

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import {AboutSection, Avatar, Title, Text, SubTitle} from './style';
-import {SectionIntro, ContainerLayout, ResumeButton} from "../common";
+import { AboutSection, Avatar, Title, Text, SubTitle } from "./style"
+import { SectionIntro, ContainerLayout, ResumeButton } from "../common"
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -13,6 +13,11 @@ const About = () => {
           }
         }
       }
+      site {
+        siteMetadata {
+          author
+        }
+      }
     }
   `)
   return (
@@ -21,16 +26,30 @@ const About = () => {
         <ContainerLayout>
           <AboutSection>
             <div>
-              <Avatar fluid={data.placeholderImage.childImageSharp.fluid} alt="user photo" />
+              <Avatar
+                fluid={data.placeholderImage.childImageSharp.fluid}
+                alt="user photo"
+              />
               <SubTitle> Front End Developer</SubTitle>
-            </div> 
+            </div>
             <div>
-              <Title> Hello, I’m AbdAli </Title>
-              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">North Africa</b> living in Casablanca. </Text>
-              <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
-              <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+              <Title> Hello, Im {data.site.siteMetadata.author} </Title>
+              <Text>
+                {" "}
+                I'm a Full Stack Developer hailing from{" "}
+                <b className="text-primary lined-link">India</b> living in
+                TamilNadu.{" "}
               </Text>
-              <ResumeButton href="resume.pdf" target="_blank"> Download resume </ResumeButton>
+              <Text>
+                {" "}
+                I love working with modern technologies, building and designing
+                awesome projects. I prefer minimalistic & clean designs with
+                strong user experience.
+              </Text>
+              <ResumeButton href="resume.pdf" target="_blank">
+                {" "}
+                Download resume{" "}
+              </ResumeButton>
             </div>
           </AboutSection>
         </ContainerLayout>
@@ -38,6 +57,5 @@ const About = () => {
     </>
   )
 }
-
 
 export default About
