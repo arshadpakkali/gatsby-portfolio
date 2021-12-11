6e5a7a809cab4ccc373a428ffd81d3e2b4f6c139
Arshad Pakkali <arshadpakkali@gmail.com>
2021-12-11 12:37:02 +0530

    added: setup for gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 module.exports = {
+  pathPrefix: "/gatsby-portfolio",
   siteMetadata: {
     title: data.SiteTitle,
     description: data.SiteDescription,
